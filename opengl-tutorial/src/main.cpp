9d421ee0444bb7d35b8115dd9ad22307e46f796b
Lam <shun_min@hotmail.com>
2023-03-14 07:53:31 +0800

    Add shader compiling

--- opengl-tutorial/src/main.cpp
+++ opengl-tutorial/src/main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+//#include <glm/glm.hpp>
+//#include <glm/gtc/matrix_transform.hpp>
+//#include <glm/gtc/type_ptr.hpp>
+
+#include <fstream>
+#include <sstream>
+#include <streambuf>
+#include <string>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+std::string loadShaderSrc(const char* filename);
 
 int main() {
     std::cout << "Hello World" << std::endl;
     
+    int success;
+    char infoLog[512];
+
+    //glm test
+    //glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+    //glm::mat4 trans = glm::mat4(1.0f); // identity matrix
+    //trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 1.0f));
+    //vec = trans * vec; // matrix has to be on the left
+    //std::cout << vec.x << ' ' << vec.y << ' ' << vec.z; 
+
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -32,6 +51,29 @@ int main() {
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    /*
+    shader
+    */
+
+    //compile vertex shader glsl
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    std::string vertShaderSrc = loadShaderSrc("assets/vertex_core.glsl");
+    const GLchar* vertShader = vertShaderSrc.c_str();
+    glShaderSource(vertexShader, 1, &vertShader, NULL);
+    glCompileShader(vertexShader);
+
+    // catch error
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "Error with vertex shader comp:" << std::endl << infoLog << std::endl;
+    }
+    
+    // compile fragement shade
+    unsigned int fragmentShader;
+
+
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
@@ -53,4 +95,24 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 void processInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+}
+
+std::string loadShaderSrc(const char* filename) {
+    std::ifstream file;
+    std::stringstream buf;
+    std::string ret = "";
+
+    file.open(filename);
+
+    if (file.is_open()) {
+        buf << file.rdbuf();
+        ret = buf.str();
+    }
+    else {
+        std::cout << "Could not open" << filename << std::endl;
+    }
+
+    file.close();
+
+    return ret;
 }
